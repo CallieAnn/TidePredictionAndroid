3233ad3b93ce2876bc5d999d3270c65f9c528613
CallieAnn <dedorec@my.lanecc.edu>
2018-11-03 19:21:52 -0700

    tide predictions displayed

---
+++ TidePrediction/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace TidePrediction
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        List<IDictionary<string, object>> tideList;
+        PredictionItem[] prediction;
+        PredictionItem singlePrediction;
+        string date;
+        string day;
+        string time;
+        string height;
+        string hiLow;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            XmlTideFileParser parser = new XmlTideFileParser(Assets.Open(@"9434032_annual.xml"));
+
+            tideList = parser.TideList;
+
+            prediction = new PredictionItem[tideList.Count];
+
+            int i = 0;
+            foreach(JavaDictionary<string, object> d in tideList)
+            {
+                date = (string)d["date"];
+                day = (string)d["day"];
+                time = (string)d["time"];
+                height = (string)d["pred_in_ft"];
+                hiLow = (string)d["highlow"];
+                singlePrediction = new PredictionItem(date, day, time,
+                    height, hiLow);
+
+                prediction[i] = singlePrediction;
+                i++;
+            }
+
+            ListAdapter = new ArrayAdapter<PredictionItem>(this, Android.Resource.Layout.SimpleListItem1, prediction );
+
+            
+
+            
+
+            
+            
+
+            
+        }
+    }
+}
---
+++ TidePrediction/PredictionItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TidePrediction
+{
+    public class PredictionItem
+    {
+        public string Date { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public string Height { get; set; }
+        public string Hi_Low { get; set; }
+
+        public PredictionItem(string dt, string d, string t,
+            string h, string h_l)
+        {
+            Date = dt;
+            Day = d;
+            Time = t;
+            Height = h;
+            Hi_Low = h_l;
+        }
+
+        public override string ToString()
+        {
+            return Day + " " + Date + "\n" + Time + " " + Hi_Low;
+        }
+    }
+}
