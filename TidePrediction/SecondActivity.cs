08fdcb6d04ec036f4a4b74a339b017338cf39933
CallieAnn <dedorec@my.lanecc.edu>
2018-11-20 22:50:10 -0800

    google api client connection fails

--- TidePrediction/SecondActivity.cs
+++ TidePrediction/SecondActivity.cs
@@ -13,21 +13,22 @@ using System;
 using Android.Gms.Common;
 using Android.Gms.Location;
 using Android.Gms.Common.Apis;
-using Android.Locations;
 using Android.Util;
 using System.Threading.Tasks;
+using Android.Locations;
 
 namespace TidePrediction
 {
     [Activity(Label = "Second", ParentActivity = typeof(MainActivity))]
-    public class SecondActivity : ListActivity
+    public class SecondActivity : ListActivity, GoogleApiClient.IConnectionCallbacks,
+        GoogleApiClient.IOnConnectionFailedListener, Android.Gms.Location.ILocationListener
+
     {
         PredictionItem[] tidesArray;
         const string CITY = "City";
         const string DATE = "Date";
         const string TODAY = "isToday";
-        //GoogleApiClient googleApiClient;
-        FusedLocationProviderClient fusedLocationProviderClient;
+        GoogleApiClient googleApiClient;
 
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -54,38 +55,37 @@ namespace TidePrediction
             // Open the database
             db = new SQLiteConnection(dbPath);
 
-            if(today == true)
+            if (today == true)
             {
-                fusedLocationProviderClient = LocationServices.GetFusedLocationProviderClient(this);
-                if (IsGoogleServicesInstalled() == true)
+                googleApiClient = new GoogleApiClient.Builder(this, this, this)
+                    .AddApi(LocationServices.API).Build();
+
+                googleApiClient.Connect();
+
+                
+            }
+
+            else
+            {
+                var tides = (from t in db.Table<PredictionItem>()
+                             where (t.City == city)
+                             && (t.Date == date)
+                             select t).ToList();
+                int count = tides.Count;
+                tidesArray = new PredictionItem[count];
+
+                //put tides from database into array for ListAdapter to use
+                for (int i = 0; i < count; i++)
                 {
-                    LocationRequest locationRequest = new LocationRequest()
-                                  .SetPriority(100)
-                                  .SetInterval(1000)
-                                  .SetFastestInterval(500);
-                    GetLastLocationFromDevice();
-                    //GetLocation(locationRequest, ) MAKE INTERFACE to implement LocationCallback
-
+                    tidesArray[i] =
+                        tides[i];
                 }
+
+                ListAdapter = new TideAdapter<PredictionItem>(this, Android.Resource.Layout.SimpleListItem1, tidesArray);
+
+                ListView.FastScrollEnabled = true;
             }
-
-            var tides = (from t in db.Table<PredictionItem>()
-                         where (t.City == city)
-                         && (t.Date == date)
-                         select t).ToList();
-            int count = tides.Count;
-            tidesArray = new PredictionItem[count];
-
-            //put tides from database into array for ListAdapter to use
-            for (int i = 0; i < count; i++)
-            {
-                tidesArray[i] =
-                    tides[i];
-            }
-
-            ListAdapter = new TideAdapter<PredictionItem>(this, Android.Resource.Layout.SimpleListItem1, tidesArray);
-
-            ListView.FastScrollEnabled = true;
+           
 
         }
 
@@ -98,48 +98,27 @@ namespace TidePrediction
             Android.Widget.Toast.MakeText(this, measurement, Android.Widget.ToastLength.Short).Show();
         }
 
-        public bool IsGoogleServicesInstalled()
+        public void OnConnected(Bundle connectionHint)
         {
-            var queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-
-            if (queryResult == ConnectionResult.Success)
-            {
-                Log.Info("SecondActivity", "Google Play Services is installed on this device.");
-                return true;
-            }
-
-            if (GoogleApiAvailability.Instance.IsUserResolvableError(queryResult))
-            {
-                // Check if there is a way the user can resolve the issue
-                var errorString = GoogleApiAvailability.Instance.GetErrorString(queryResult);
-                Log.Error("MainActivity", "There is a problem with Google Play Services on this device: {0} - {1}",
-                          queryResult, errorString);
-
-                // Alternately, display the error to the user.
-            }
-
-            return false;
+                Android.Locations.Location location = LocationServices.FusedLocationApi.GetLastLocation(googleApiClient);
+                Log.Info("LastLocation", location.ToString());
         }
 
-        public async Task GetLastLocationFromDevice()
+        public void OnConnectionSuspended(int cause)
         {
-            // This method assumes that the necessary run-time permission checks have succeeded.
-            Android.Locations.Location location = await fusedLocationProviderClient.GetLastLocationAsync();
-
-            if (location == null)
-            {
-                // Seldom happens, but should code that handles this scenario
-            }
-            else
-            {
-                // Do something with the location 
-                Log.Debug("Sample", "The latitude is " + location.Latitude);
-            }
+            throw new NotImplementedException();
         }
 
-        //public async Task GetLocation(locReq, locCallBack)
-        //{
-        //await fusedLocationProviderClient.RequestLocationUpdatesAsync(locationRequest, locationCallback);
-        // }
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            var r = result.HasResolution;
+            Log.Debug("Has Resolution", r.ToString());
+            Android.Widget.Toast.MakeText(this, "Connection Failed", Android.Widget.ToastLength.Short).Show();
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
